f2eba2dae1d0d47a6d8153e61aad1c17df897f45
osbowski <osobiesam@gmail.com>
2021-09-28 10:06:52 +0200

    add-todo-form

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <input type="text" placeholder="To do" v-model="todoName" />
+    <label for="deadline">Deadline for this:</label>
+    <input type="date" name="deadline" v-model="deadline" />
+    <button @click.prevent="onAddTodo">Add to list</button>
+  </form>
+</template>
+
+<script>
+
+
+import { ref } from "vue";
+export default {
+  setup() {
+    const todoName = ref("");
+    const deadline = ref(null);
+
+    const onAddTodo = ()=>{
+        const todo = {
+            todoName:todoName.value,
+            deadline:Date.parse(deadline.value),
+            createdAd:Date.now()
+        }
+        console.log(todo)
+    }
+
+    return {
+      todoName,
+      deadline,
+      onAddTodo
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <h1>Hello vue</h1>
+    <TodoForm />
     <TodosList />
   </div>
 </template>
 
 <script>
-import TodosList from '../components/TodosList.vue'
+import TodosList from '../components/TodosList.vue';
+import TodoForm from '../components/TodoForm.vue';
 export default {
   name: 'Home',
   components:{
     TodosList,
+    TodoForm,
   }
 }
 </script>
