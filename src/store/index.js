ff4487f1e7ba9ad8bc4b3b788d7ace0703db44cf
osbowski <osobiesam@gmail.com>
2021-09-29 13:57:39 +0200

    add-todo-to-db

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,9 @@ export default createStore({
         (todo) => todo.id === payload.id
       );
       if (existingElement !== undefined) {
-        const indexToChange = state.todos.findIndex(todo=>todo.id === payload.id);
+        const indexToChange = state.todos.findIndex(
+          (todo) => todo.id === payload.id
+        );
         state.todos[indexToChange] = payload;
       } else {
         state.todos.push(payload);
@@ -21,7 +23,12 @@ export default createStore({
     },
   },
   actions: {
-    addTodoToList(context, payload) {
+    async addTodoToList(context, payload) {
+      await fetch(`https://osb-todo-default-rtdb.firebaseio.com/todos.json`, {
+        method: "POST",
+        body: JSON.stringify(payload),
+      });
+
       context.commit("addTodoToList", payload);
     },
     removeTodoFromList(context, payload) {
