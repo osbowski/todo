a378fb5f740d5c4e23426be8d7c6fc8211436eec
osbowski <osobiesam@gmail.com>
2021-10-04 10:53:52 +0200

    create-login-signin-form

---
+++ src/components/UserAuth.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="login">
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" autocomplete="username" v-model="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input id="password" type="password" autocomplete="current-password" v-model="password">
+        </div>
+        <div>
+            <button>Login</button>
+            <button>Sign up</button>
+        </div>
+    </form>
+</template>
+
+<script>
+import {ref} from 'vue';
+export default {
+    setup() {
+        const email = ref('');
+        const password = ref('');
+
+        const login = ()=>{
+            const user = {
+                email:email.value,
+                password:password.value
+            }
+            console.log('USER:',user)
+        }
+
+        return{
+            email,
+            password,
+            login
+        }
+    },
+}
+</script>
