54543a034b101254ef573477c036fea030c2fdc0
osbowski <osobiesam@gmail.com>
2021-10-04 14:08:45 +0200

    login-logut-by-firebase

--- src/components/UserAuth.vue
+++ src/components/UserAuth.vue
@@ -1,40 +1,66 @@
 <template>
-    <form @submit.prevent="login">
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" autocomplete="username" v-model="email">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input id="password" type="password" autocomplete="current-password" v-model="password">
-        </div>
-        <div>
-            <button>Login</button>
-            <button>Sign up</button>
-        </div>
-    </form>
+  <form @submit.prevent="submitAuthForm">
+    <div>
+      <label for="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        autocomplete="username"
+        v-model.trim="email"
+      />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        autocomplete="current-password"
+        v-model.trim="password"
+      />
+    </div>
+      <button>{{submitButtonCaption}}</button>
+      <button type="button" @click="switchAuthMode">{{switchModeButtonCaption}}</button>
+  </form>
 </template>
 
 <script>
-import {ref} from 'vue';
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
 export default {
-    setup() {
-        const email = ref('');
-        const password = ref('');
+  setup() {
+    const store = useStore();
+    const email = ref("");
+    const password = ref("");
+    const mode = ref("login");
 
-        const login = ()=>{
-            const user = {
-                email:email.value,
-                password:password.value
-            }
-            console.log('USER:',user)
-        }
+    const submitButtonCaption = computed(()=> mode.value === 'login' ? 'Login' : 'Singup');
+    const switchModeButtonCaption = computed(()=> mode.value === 'login' ? 'or Sing up' : 'or Log in');
 
-        return{
-            email,
-            password,
-            login
-        }
-    },
-}
+    const switchAuthMode = ()=> {
+        mode.value = mode.value==='login' ? 'signup' : 'login'
+    };
+
+    const submitAuthForm = () => {
+      const user = {
+        email: email.value,
+        password: password.value,
+      };
+      if(mode.value === 'login'){
+           store.dispatch("login", user);
+      }else{
+           store.dispatch("signup", user);
+      }
+    };
+
+    return {
+      email,
+      password,
+      mode,
+      submitButtonCaption,
+      switchModeButtonCaption,
+      switchAuthMode,
+      submitAuthForm,
+    };
+  },
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,8 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     todos: [],
+    userId:null,
+    token:null
   },
   mutations: {
     addTodoToList(state, payload) {
@@ -21,8 +23,63 @@ export default createStore({
     removeTodoFromList(state, payload) {
       state.todos = state.todos.filter((todo) => todo.id !== payload);
     },
+    setUser(state,payload){
+      state.userId = payload.useId;
+      state.token = payload.token;
+    }
   },
   actions: {
+    login(context, payload) {
+      return context.dispatch("auth", {
+        ...payload,
+        mode: "login",
+      });
+    },
+    signup(context, payload) {
+      return context.dispatch("auth", {
+        ...payload,
+        mode: "signup",
+      });
+    },
+
+    async auth(context, payload) {
+      let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCd6wnwZ6_0z_Mrrma0Qd2YIOx6TsG2KbQ`;
+      if (payload.mode === "signup") {
+        url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCd6wnwZ6_0z_Mrrma0Qd2YIOx6TsG2KbQ`;
+      }
+
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      });
+
+      const responseData = await response.json();
+      const expires = +responseData.expiresIn * 1000;
+      const expirationDate = new Date().getTime() + expires;
+      localStorage.setItem("token", responseData.idToken);
+      localStorage.setItem("userId", responseData.localId);
+      localStorage.setItem("tokenExpiration", expirationDate);
+
+      context.commit('setUser',{
+        token:responseData.idToken,
+        userId:responseData.localId
+      })
+    },
+    logout(context){
+      localStorage.removeItem('token')
+      localStorage.removeItem('userId')
+      localStorage.removeItem('tokenExpiration')
+
+      context.commit('setUser',{
+        token:null,
+        userId:null
+      })
+      
+    },
     async addTodoToList(context, payload) {
       if (payload.id === null) {
         const response = await fetch(
