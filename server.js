1adbf702f6c1a2719e87cde8c1f6d5ab61c54683
osbowski <osobiesam@gmail.com>
2021-10-08 13:23:12 +0200

    add-connect-history-api-fallback

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
 const history = require('connect-history-api-fallback')
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+app.use(staticFileMiddleware)
 
 app.use(history({
     disableDotRule: true,
     verbose: true
 }))
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'))
+  })
+  
 const port = process.env.PORT || 3000;
 app.listen(port);
