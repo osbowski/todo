4548469af16ec95e69da54abae230a228f306d8e
osbowski <osobiesam@gmail.com>
2021-10-08 13:12:12 +0200

    heroku-deploy-configure-2

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
+const history = require('connect-history-api-fallback')
 
 app.use(history({
+    disableDotRule: true,
     verbose: true
 }))
 app.use(serveStatic(path.join(__dirname, 'dist')));
